d9d6e329eef619810f85b423f2cfae05b4bac105
superwm007 <31275019+superwm007@users.noreply.github.com>
2022-10-25 21:37:25 +0200

    ver 2

--- WASM/Shared/Employee.cs
+++ WASM/Shared/Employee.cs
@@ -13,10 +13,13 @@ namespace WASM.Shared
         [StringLength(10, ErrorMessage = "This is too long name!!!")]
         public string LastName { get; set; }
         public DateTime? BirthDate { get; set; }
+        [EmailAddress (ErrorMessage="This must be an email")]
         public string? Email { get; set; }
+        [Phone(ErrorMessage = "This must be an Phone")]
         public string? PhoneNumber { get; set; }
         public int CountryId { get; set; }
         public virtual Country ?Country { get; set; }
+        [StringLength(100, ErrorMessage = "This is too long name max 100 !!!")]
         public string? Comment { get; set; }
         public DateTime? JoinedDate { get; set; }
         public DateTime? ExitDate { get; set; }
